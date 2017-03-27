3fa8d11254d4f6d104dea635c2d04ed86b1ceffe
Trevor <Trevor@DESKTOP-71B80IP>
2017-03-27 10:16:37 +0100

    Merge origin/master
    Conflicts:
    	fetchView.js

---
+++ PhpProject1/walkCycle/walkCycleController.js
@@ -0,0 +1,30 @@
+/* 
+ * © 2017-03: Florian Haimerl
+ */
+
+/*jslint node: true, browser: true */
+/*global WalkCycleModel*/
+/*global WalkCycleView*/
+"use strict";
+
+var model = new WalkCycleModel();
+var view = new WalkCycleView();
+
+function WalkCycleController() {
+    this.init = function () {
+        var interval_id = setInterval(function () {
+            if(!model.PositionIsSet()) {
+                return;
+            }
+            view.setGetDogCallback(model.getDog);
+            model.init();
+            model.setSetModeCallback(view.setMode);
+            model.setMode("pick_up");
+            clearInterval(interval_id);
+        },250);
+    };
+}
+
+
+var controller = new WalkCycleController();
+window.addEventListener("load", controller.init);
